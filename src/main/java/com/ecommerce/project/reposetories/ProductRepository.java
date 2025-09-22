b8d2b90f22989e755accbe3c949b17bd0abf7d40
Lior <liorer@mta.ac.il>
2025-09-22 13:06:34 +0300

    ..

--- src/main/java/com/ecommerce/project/reposetories/ProductRepository.java
+++ src/main/java/com/ecommerce/project/reposetories/ProductRepository.java
@@ -9,9 +9,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageDetails);
     Page<Product> findByProductNameLikeIgnoreCase(String keyword, Pageable pageDetails);
     Page<Product> findByUser(User user, Pageable pageDetails);
+    Optional<Product> findByCategoryAndProductNameIgnoreCase(Category category, String productName);
+
+
 }
